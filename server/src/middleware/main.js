894158ae8b1a653ec7d3a18e826873e8610ef675
Tony Jiang <white87332@gmail.com>
2017-03-30 14:59:28 +0800

    mark webpack build info

--- server/src/middleware/main.js
+++ server/src/middleware/main.js
@@ -47,7 +47,7 @@ export default function (app)
         const webpackHotMiddleware = require('webpack-hot-middleware');
         app.use(webpackDevMiddleware(compiler,
             {
-                noInfo: false, publicPath: config.output.publicPath,
+                noInfo: true, publicPath: config.output.publicPath,
             }));
         app.use(webpackHotMiddleware(compiler));
     }
